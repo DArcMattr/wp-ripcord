8c18d9b6a9af68fc6904bb2da6a02b3b923a1c1a
David Arceneaux <david@davidthemachine.org>
2018-04-30 18:54:03 -0700

    document purpose of file

--- cli-vars-sample.php
+++ cli-vars-sample.php
@@ -1,3 +1,4 @@
 <?php
+// Make it so that your high-privilege local user can work with wp-cli.
 define( 'DB_USER', 'WordPress DB username goes here' );
 define( 'DB_PASSWORD', 'Password for the DB user goes here' );
